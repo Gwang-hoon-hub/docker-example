fa67f17b893bc9a13e132707befdfeed90ca92cd
panghoon <leegwanghoon95@gmail.com>
2022-04-14 18:30:10 +0900

    logback Initial Commit

---
+++ src/main/java/com/pangpang/dockerexample/service/TestService.java
@@ -0,0 +1,10 @@
+package com.pangpang.dockerexample.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
---
+++ src/test/java/com/pangpang/dockerexample/logback/TestController.java
@@ -0,0 +1,21 @@
+package com.pangpang.dockerexample.logback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class TestController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/test")
+    public void logbackTest(){
+        logger.trace("Trace Level 테스트");
+        logger.debug("Debug Level 테스트");
+        logger.info("Info Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("Error Level 테스트");
+    }
+}
