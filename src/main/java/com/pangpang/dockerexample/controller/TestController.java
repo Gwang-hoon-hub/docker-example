87383803cb965b9958bb3947a3aeeabbd2099b25
panghoon <leegwanghoon95@gmail.com>
2022-04-15 14:41:29 +0900

    [FEAT] : add Logback err file

--- src/main/java/com/pangpang/dockerexample/controller/TestController.java
+++ src/main/java/com/pangpang/dockerexample/controller/TestController.java
@@ -3,6 +3,7 @@ package com.pangpang.dockerexample.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +12,15 @@ public class TestController {
 
     @GetMapping("/test")
     public void logbackTest(){
+        logger.trace("Trace Level 테스트");
+        logger.debug("Debug Level 테스트");
+        logger.info("Info Level 테스트");
+        logger.warn("Warn Level 테스트");
+        logger.error("Error Level 테스트");
+    }
+
+    @PostMapping("/test")
+    public void logbackTest1(){
         logger.trace("Trace Level 테스트");
         logger.debug("Debug Level 테스트");
         logger.info("Info Level 테스트");
