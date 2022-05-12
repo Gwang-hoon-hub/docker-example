aafcb2ebab8ded97e05d3219a198a49b864fe20b
panghoon <leegwanghoon95@gmail.com>
2022-05-12 21:44:21 +0900

    [FEAT] : add redis configure in yml

--- src/main/java/com/pangpang/dockerexample/config/RedisConfig.java
+++ src/main/java/com/pangpang/dockerexample/config/RedisConfig.java
@@ -1,23 +1,30 @@
 package com.pangpang.dockerexample.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@RequiredArgsConstructor
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig{
 //    @Value("${spring.redis.host}")
-    private String host;
-
+//    private String host;
 //    @Value("${spring.redis.port}")
-    private int port;
+//    private int port;
+
+    private final RedisProperties redisProperties;
 
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+//        return new LettuceConnectionFactory(host, port);
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
     }
 
     @Bean
